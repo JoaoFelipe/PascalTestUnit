cd4c9821b9d3e6ac50f558066fc09167e010ec85
Joao Felipe Nicolaci Pimentel <joao@joao-R590.(none)>
2011-03-28 13:55:17 -0300

    README

--- TestProgram.pas
+++ TestProgram.pas
@@ -4,17 +4,17 @@ uses TestUnit;
 procedure TestSimple;
 begin
   Assert(1 = 1);
-	Assert(FloatEquals(1, 1));
+  Assert(FloatEquals(1, 1));
 end;
 
 procedure TestFail;
 begin
   InitTest('1 = 0 deve falhar');
   Assert(1 = 0);
-	Assert(FloatEquals(1, 0));
+  Assert(FloatEquals(1, 0));
 
-	InitTest('3 = 2 deve falhar');
-	Assert(3 = 2);
+  InitTest('3 = 2 deve falhar');
+  Assert(3 = 2);
 end;
 
 
